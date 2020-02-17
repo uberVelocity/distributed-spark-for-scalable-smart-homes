8fda1d7215f0e392e4ed4133be545fbe6c3a7520
ubervelocity <mihaitu.popescu@gmail.com>
2020-02-17 10:11:33 +0100

    added UNIX epoch datetime to appliance sensor

--- sensors/appliance_app/appliance/simulate_appliance.py
+++ sensors/appliance_app/appliance/simulate_appliance.py
@@ -1,5 +1,9 @@
 from appliance import Appliance
 from time import sleep
+from datetime import datetime
+
+def unix_time_millis(dt):
+    return (dt - epoch).total_seconds() * 1000.0
 
 if __name__ == '__main__':
     watt_params = {
@@ -17,15 +21,17 @@ if __name__ == '__main__':
 
     dev_id = 1
     device = Appliance(dev_id, watt_params, temp_params)
-
+    
     t = 0
     while True:
         if t is 50 or device.on_state is False:
             break
 
+        # TODO generate a timestamp that adheres to the UNIX Epoch https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+        dt = datetime.now()
         watts = device.compute_wattage(t)
         temperature = device.compute_temperature(t)
-
+        print(f"Device {device.id}: time({t}) = {dt}")
         print(f"Device {device.id}: wattage({t}) = {watts}")
         print(f"Device {device.id}: temperature({t}) = {temperature}")
 
