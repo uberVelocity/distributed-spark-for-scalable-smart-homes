5cd14b891e09dc7d57378d4d64373b031fcdf79f
ubervelocity <mihaitu.popescu@gmail.com>
2020-02-20 10:45:44 +0100

    unified

--- sensors/db_interface/server/index.js
+++ sensors/db_interface/server/index.js
@@ -2,8 +2,36 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const PackageService = require("../services/PackageService");
+const {Kafka} = require('kafkajs')
+
 
 const app = express();
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['kafka:9092', 'kafka2:9092', 'kafka3:9092']
+});
+
+const consumer = kafka.consumer({groupId: 'db-interface'});
+
+const run = async() => {
+    await consumer.connect()
+    await consumer.subscribe({topic: 'historical', fromBeginning: true});
+    
+    await consumer.run({
+        eachMessage: async ({topic, partition, message}) => {
+            // For each message, you might want to insert it into the db.
+            console.log({
+                partition,
+                offset: message.offset,
+                value: message.value.toString(),
+            })
+        }
+    });
+}
+
+
+run().catch(console.error);
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -13,6 +41,6 @@ const insert = require('../routes/api/insert');
 
 app.use('/api/insert', insert);
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4004;
 
 app.listen(port, () => console.log(`Test sensor started on port ${port}`));
