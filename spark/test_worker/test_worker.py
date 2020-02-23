ef1354216f2b4134ec0fc54fdcd32653632ca5b8
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-02-23 22:39:51 +0100

    started work on some spark stuff

--- spark/test_worker/test_worker.py
+++ spark/test_worker/test_worker.py
@@ -1,35 +1,24 @@
-from kafka import KafkaConsumer
-from kafka.errors import NoBrokersAvailable
-from json import loads
-from time import sleep
+"""
+Spark packages need to be called at job commission, which are then available in the script by some package name, through
+the usual import statements
+"""
+import pyspark_cassandra
 
 if __name__ == '__main__':
+    conf = SparkConf() \
+        .setAppName("PySpark Cassandra Test") \
+        .setMaster("spark://spark-master:7077") \
+        .set("spark.cassandra.connection.host", "cas-1")
 
-    consumer = None
-    while not consumer:  # Continuously try to connect to broker
-        try:
-            # To consume latest messages and auto-commit offsets
-            consumer = KafkaConsumer('historical',
-                                     group_id='spark_test_worker',
-                                     bootstrap_servers=['kafka:29091'],
-                                     key_deserializer=lambda m: m.decode(),
-                                     value_deserializer=lambda m: loads(m.decode('ascii')))
-        except NoBrokersAvailable:
-            print('Broker not yet available, sleeping', flush=True)
-            sleep(5)
+    sc = CassandraSparkContext(conf=conf)
 
-    while True:
-        topics = consumer.poll()
-        print(topics.keys(), flush=True)
+    sc \
+        .cassandraTable("keyspace", "table") \
+        .select("col-a", "col-b") \
+        .where("key=?", "x") \
+        .filter(lambda r: r["col-b"].contains("foo")) \
+        .map(lambda r: (r["col-a"], 1)
+             .reduceByKey(lambda a, b: a + b)
+             .collect()
 
-        try:
-            assert len(topics.keys()) != 0
-
-            for topic in topics.keys():
-                for record in topics[topic]:
-                    print(record.value, flush=True)
-
-        except AssertionError:
-            print('No messages available, sleeping', flush=True)
-
-        sleep(5)
+    pass
