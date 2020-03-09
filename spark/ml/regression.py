10847fc869a9d2811562f1b513b77efe265ebe46
ubervelocity <mihaitu.popescu@gmail.com>
2020-03-09 14:13:45 +0100

    full map fitler reduce

--- spark/ml/regression.py
+++ spark/ml/regression.py
@@ -3,6 +3,7 @@ import os
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import mean as _mean, stddev as _stddev, col
+import pyspark.sql.functions as f
 
 def load_and_get_table_df(keys_space_name, table_name):
     """
@@ -31,29 +32,33 @@ if __name__ == '__main__':
     sql_context = SQLContext(spark_context)  # needed to be able to query data.
 
     heaters = load_and_get_table_df('household', 'heatersensor')
+    heaters.show()
 
-    # Store the average of the "temperature" column
+    # Compute the mean and std of the 'temp' column
     df_stats = heaters.select(
     _mean(col('temp')).alias('mean'),
     _stddev(col('temp')).alias('std')
     ).collect()
 
-    # Compute mean and std
+    # Store mean and std
     mean = df_stats[0]['mean']
     std = df_stats[0]['std']
 
-    # Collect all Rows of 'temp' column
-    rows = heaters.select('temp').collect()
-    vals = []
-    for item in rows:
-        vals.append(item[0])
+    # Set z-score threshold
+    threshold = 0.4
 
+    # Compute z-scores of 'temp' column
     z_scores = heaters.select('temp').rdd.map(lambda x:((x[0] - mean) / std, 1)).toDF()
     print('z_scores df: ', z_scores.show())
-    
-    # troublesome = filter(lambda x: x > 0.01, z_scores)
-    # print('FILTER: ', list(troublesome))
 
-    heaters.show()
+    # Filter z-scores values above threshold
+    filtered = z_scores.filter(f.col('_1') > threshold)
+    print('FILTER: ', filtered.show())
+
+    # Reduce (count how many)
+    reduced = filtered.rdd.reduce(lambda a, b: ('sum', a[1] + b[1]))[1]
+    print('REDUCED: ', reduced)
+    
+    # Finish
     spark_context.stop()
 
