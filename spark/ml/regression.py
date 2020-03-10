20da6400ee14a5a226a5494a7c8ce4d8fdf27a3b
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-03-10 09:56:40 +0100

    cleaned up mean/std computation

--- spark/ml/regression.py
+++ spark/ml/regression.py
@@ -5,6 +5,7 @@ from pyspark.sql import SQLContext
 from pyspark.sql.functions import mean as _mean, stddev as _stddev, col
 import pyspark.sql.functions as f
 
+
 def load_and_get_table_df(keys_space_name, table_name):
     """
     Reads a table from a Cassandra keyspace as a PySpark DataFrame.
@@ -17,6 +18,7 @@ def load_and_get_table_df(keys_space_name, table_name):
         .options(table=table_name, keyspace=keys_space_name)\
         .load()
     return table_df
+
 
 def z_scores(x, mean, std):
     print(x[0])
@@ -32,32 +34,24 @@ if __name__ == '__main__':
     sql_context = SQLContext(spark_context)  # needed to be able to query data.
 
     heaters = load_and_get_table_df('household', 'heatersensor')
-    heaters.show()
 
-    # Compute the mean and std of the 'temp' column
-    df_stats = heaters.select(
-    _mean(col('temp')).alias('mean'),
-    _stddev(col('temp')).alias('std')
-    ).collect()
+    heater_stats = heaters.groupBy('id').agg(f.mean('temp').alias('mean'), f.stddev('temp').alias('std')).show()
 
-    # Store mean and std
-    mean = df_stats[0]['mean']
-    std = df_stats[0]['std']
-
-    # Set z-score threshold
-    threshold = 0.4
-
-    # Compute z-scores of 'temp' column
-    z_scores = heaters.select('temp').rdd.map(lambda x:((x[0] - mean) / std, 1)).toDF()
-    print('z_scores df: ', z_scores.show())
-
-    # Filter z-scores values above threshold
-    filtered = z_scores.filter(f.col('_1') > threshold)
-    print('FILTER: ', filtered.show())
-
-    # Reduce (count how many)
-    reduced = filtered.rdd.reduce(lambda a, b: ('sum', a[1] + b[1]))[1]
-    print('REDUCED: ', reduced)
+    #
+    # # Set z-score threshold
+    # threshold = 0.4
+    #
+    # # Compute z-scores of 'temp' column
+    # z_scores = heaters.select('temp').rdd.map(lambda x:((x[0] - mean) / std, 1)).toDF()
+    # print('z_scores df: ', z_scores.show())
+    #
+    # # Filter z-scores values above threshold
+    # filtered = z_scores.filter(f.col('_1') > threshold)
+    # print('FILTER: ', filtered.show())
+    #
+    # # Reduce (count how many)
+    # reduced = filtered.rdd.reduce(lambda a, b: ('sum', a[1] + b[1]))[1]
+    # print('REDUCED: ', reduced)
     
     # Finish
     spark_context.stop()
