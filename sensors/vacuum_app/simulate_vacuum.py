df2c35578496608479e686c92d84122f7a974fcf
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-03-19 16:12:07 +0100

    Refactored vacuum code

--- sensors/vacuum_app/simulate_vacuum.py
+++ sensors/vacuum_app/simulate_vacuum.py
@@ -1,4 +1,4 @@
-from vacuum import Vacuum
+from sensor import Sensor
 from time import sleep
 from datetime import datetime
 from json import dumps
@@ -18,18 +18,18 @@ def on_send_error(excp):
 
 
 if __name__ == '__main__':
-    sleep(15)
     watt_params = {
-        'base': 6,
-        'variance': 0.2,
-        'limit': 10
+        'a': 1.2,
+        'b': 150,
+        'variance': 1,
+        'limit': 200
     }
 
     suction_params = {
-        'base': 100,
-        'variance': 2,
-        'limit': 70,
-        't_fault': 10
+        'a': -0.2,
+        'b': 100,
+        'variance': 1,
+        'limit': 80,
     }
 
     producer = None
@@ -43,31 +43,32 @@ if __name__ == '__main__':
         except NoBrokersAvailable:
             print('No brokers available, sleeping', flush=True)
             sleep(5)
-    vacuum = Vacuum(watt_params, suction_params)
+
+    lamp = Sensor(watt_params, suction_params)
 
     t = 0
     while True:
-        if t is 50 or vacuum.on_state is False:
+        if t == 50 or not lamp.on:   # break when appliance is broken or enough time has passed
             break
 
         timestamp = datetime.utcnow().timestamp()
-        watts = vacuum.compute_wattage(t)
-        suction = vacuum.compute_suction(t)
+        wattage = lamp.compute_var1(t)
+        suction = lamp.compute_var2(t)
 
-        print(f"Device {vacuum.id}: time({t}) = {timestamp}")
-        print(f"Device {vacuum.id}: wattage({t}) = {watts}")
-        print(f"Device {vacuum.id}: suction({t}) = {suction}")
+        print(f"Device {lamp.id}: time({t}) = {timestamp}", flush=True)
+        print(f"Device {lamp.id}: wattage({t}) = {wattage}")
+        print(f"Device {lamp.id}: suction({t}) = {suction}")
 
         msg = {
-            'id': vacuum.id,
+            'id': lamp.id,
             'timestamp': timestamp,
             'sensors': {
                 'suction': suction,
-                'wattage': watts,
+                'wattage': wattage,
             }
         }
 
-        # Stream data and and sleep for 4 seconds between updates
-        producer.send('sensor_data', key=vacuum.id, value=msg).add_callback(on_send_success).add_errback(on_send_error)
+        # Stream data and and sleep for 4 seconds between update.
+        producer.send('sensor_data', key=lamp.id, value=msg).add_callback(on_send_success).add_errback(on_send_error)
         t += 1
         sleep(4)
