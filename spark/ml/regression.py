65c5e0b5da743d366251daacf033d4afdfcb8a86
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-04-15 13:46:03 +0200

    Created function for getting min/max values and distributed computations per column.

--- spark/ml/regression.py
+++ spark/ml/regression.py
@@ -4,6 +4,7 @@ from pyspark.sql.functions import mean as _mean, stddev as _stddev, col
 
 import pyspark
 
+
 def load_and_get_table_df(keys_space_name, table_name):
     """
     Reads a table from a Cassandra keyspace as a PySpark DataFrame.
@@ -18,73 +19,86 @@ def load_and_get_table_df(keys_space_name, table_name):
     return table_df
 
 
-def z_score(x, mean, std):
-    return (x - mean) / std
+# def z_score(x, mean, std):
+#     return (x - mean) / std
+#
+#
+# def convert_to_zscore(df):
+#     """
+#     Converts a DataFrame to z score values.
+#     :param df: DataFrame to be converted
+#     :return: A DataFrame containing the transformed table.
+#     """
+#
+#     means = {}
+#     stds = {}
+#
+#     for column in df.schema.names[2:]:  # leave out the first two columns
+#         df_stats = df.select(
+#             _mean(col(column)).alias('mean'),  # Compute mean for column
+#             _stddev(col(column)).alias('std')  # Compute std for column
+#         ).collect()
+#
+#         means[column] = df_stats[0]['mean']
+#         stds[column] = df_stats[0]['std']
+#
+#     cols = df.schema.names[2:]  # Needs to be specified here, otherwise references to names not available.
+#
+#     # RDD used to distribute task
+#     transformed = df.rdd.map(lambda x: (x[0],
+#                                         x[1],
+#                                         z_score(x[2], means[cols[0]], stds[cols[0]]),
+#                                         z_score(x[3], means[cols[1]], stds[cols[1]])
+#                                         )
+#                              ).toDF(df.schema.names)
+#
+#     return transformed
 
 
-def convert_to_zscore(df):
+def compute_limits(df, column):
     """
-    Converts a DataFrame to z score values.
-    :param df: DataFrame to be converted
-    :return: A DataFrame containing the transformed table.
+    Computes min and max values for the given DataFrame column
+    :param df: DataFrame to be used.
+    :param column: String of the column name.
+    :return: (min value, max value)
     """
 
-    means = {}
-    stds = {}
+    # Select desired column and filter out all values higher than -1
+    df = df.select(col(column).alias('var')).filter("var > -1")
 
-    for column in df.schema.names[2:]:  # leave out the first two columns
-        df_stats = df.select(
-            _mean(col(column)).alias('mean'),  # Compute mean for column
-            _stddev(col(column)).alias('std')  # Compute std for column
-        ).collect()
+    # -1 values are excluded as these are used as sensor malfunction labeling.
+    max_val = df.max("var")
+    min_val = df.min("var")
 
-        means[column] = df_stats[0]['mean']
-        stds[column] = df_stats[0]['std']
+    print()
+    print(column)
+    print("Max: " + str(max_val) + "\nMin: " + str(min_val))
+    print()
 
-    cols = df.schema.names[2:]  # Needs to be specified here, otherwise references to names not available.
-
-    # RDD used to distribute task
-    transformed = df.rdd.map(lambda x: (x[0],
-                                        x[1],
-                                        z_score(x[2], means[cols[0]], stds[cols[0]]),
-                                        z_score(x[3], means[cols[1]], stds[cols[1]])
-                                        )
-                             ).toDF(df.schema.names)
-
-    return transformed
+    return min_val, max_val
 
 
-def partition_time_sync(partition):
-    """
-    Convert each partition's timestamps to seconds since start.
-    :param partition:
-    :return:
-    """
-    pass
+def linear_regression(df, column):
+    return 0, 0
 
 
-def get_coefficients(df):
+def get_coefficients_for(df):
     """
     Gets the a, b and limit coefficients per parameter column for use in prediction.
     :param df: DataFrame containing the training data.
     :return: List containing tuples (a, b, max, min) with linear regression coefficients and limits.
     """
 
-    results = spark_context.parallelize(range(2, len(df.schema.names)))
+    results = spark_context.parallelize(range(4, len(df.schema.names)))
+    results = results.map(
+        lambda x: (
+            df.schema.names[x],
+            linear_regression(df, df.schema.names[x]),
+            compute_limits(df, df.schema.names[x])
+        )
+    )
 
-    for column in df.schema.names[2:]:
-
-        # -1 values are excluded as these are used as sensor malfunction labeling.
-        max_val = df.filter(column + "> -1").groupby().max(column).first()["max(" + column + ")"]
-        min_val = df.filter(column + "> -1").groupby().min(column).first()["min(" + column + ")"]
-
-        # Print statements together as to not have spark statements clutter debug
-        print()
-        print(column)
-        print("Max: " + str(max_val) + "\nMin: " + str(min_val))
-        print()
-
-    return ''
+    return results
 
 
 if __name__ == '__main__':
@@ -95,13 +109,14 @@ if __name__ == '__main__':
     spark_context = SparkContext(master='spark://spark-master:7077', appName='regression', conf=spark_config)
     sql_context = SQLContext(spark_context)  # needed to be able to query data.
 
-    heaters = load_and_get_table_df('household', 'heatersensor')
+    heaters = load_and_get_table_df('household', 'heaters')
     heaters.show()
 
-    parameters = get_coefficients(heaters)
+    parameters = get_coefficients_for(heaters)
 
-    # heaters = convert_to_zscore(heaters)
-    # heaters.show()
+    print()
+    print(parameters)
+    print()
 
     # Finish
     spark_context.stop()
